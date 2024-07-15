d63ea8271e4d0d2f3601dad21398e7f0813f273d
Artur Melo <arturmcatunda@gmail.com>
2024-07-15 17:42:26 +0000

    added movement commands to CLIModule

--- src/components/CLIModule/CLIModule.tsx
+++ src/components/CLIModule/CLIModule.tsx
@@ -1,29 +1,40 @@
 import { useEffect, useState } from "react";
+import { checkCommands } from "./CLICommands";
 import { LoremIpsum } from "./LoremIpsum";
 
-type CommandType = {
+type CommandLineType = {
+  command: string;
   text: string;
   currentText: string;
   textRollingIndex: number;
 };
 
 export default function CLIModule() {
-  var text = LoremIpsum;
-  var delay = 25;
+  //var text = LoremIpsum;
+  var delay = 50;
 
-  const [commandHistory, setCommandHistory] = useState<CommandType[]>([]);
+  const [commandHistory, setCommandHistory] = useState<CommandLineType[]>([]);
   const [reloadAux, setReloadAux] = useState(0);
 
   const addCommand = () => {
     var input: HTMLInputElement;
     input = document.getElementById("input")! as HTMLInputElement;
 
-    setCommandHistory([
-      { text: input.value, currentText: "", textRollingIndex: -1 },
-      ...commandHistory,
-    ]);
+    if (input.value.match(/[^\s]/g) != null) {
+      setCommandHistory([
+        {
+          command: input.value,
+          text: checkCommands(input.value),
+          currentText: "",
+          textRollingIndex: -1,
+        },
+        ...commandHistory,
+      ]);
 
-    input.value = "";
+      console.log(input.value);
+
+      input.value = "";
+    }
   };
 
   const onSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
@@ -50,12 +61,36 @@ export default function CLIModule() {
 
   return (
     /* ----- TERMINAL WRAPPER ----- */
-    <div className="flex flex-col-reverse w-full h-full bg-black border-green-500 border-solid border-2 p-2 gap-2 overflow-y-scroll no-scrollbar">
+    <div
+      onClick={(e: any) => {
+        e.preventDefault();
+
+        document.getElementById("input")!.focus();
+
+        return false;
+      }}
+      className="flex flex-col-reverse w-full h-full bg-black select-none border-green-500 border-solid border-2 p-2 gap-2 overflow-y-scroll no-scrollbar"
+    >
       {/* ----- INPUT ----- */}
       <form className="flex m-0" onSubmit={onSubmitForm}>
         <input
           id="input"
-          className="flex flex-row w-full h-content px-2 focus:outline-none bg-black text-green-500 border-green-500 border-solid border-2"
+          type="text"
+          spellCheck="false"
+          autoFocus
+          onPaste={(e: any) => {
+            e.preventDefault();
+            return false;
+          }}
+          onCut={(e: any) => {
+            e.preventDefault();
+            return false;
+          }}
+          onDrop={(e: any) => {
+            e.preventDefault();
+            return false;
+          }}
+          className="flex flex-row w-full h-content px-2 pointer-events-none focus:outline-none bg-black text-green-500 border-green-500 border-solid border-2"
         ></input>
         {/* <button onClick={addCommand} className="flex bg-white px-2">
           send
@@ -67,8 +102,9 @@ export default function CLIModule() {
         return (
           <div
             key={i == 0 ? reloadAux : i}
-            className="flex flex-row w-full h-content px-2 text-left text-wrap break-all text-green-500 border-green-500 border-solid border-2"
+            className="flex flex-col w-full h-content px-2 text-left text-wrap break-all text-green-500 whitespace-pre border-green-500 border-solid border-2"
           >
+            <div className="flex">{`${v.command}`}</div>
             {i == 0 ? v.currentText + " <" : v.text + " <"}
           </div>
         );
